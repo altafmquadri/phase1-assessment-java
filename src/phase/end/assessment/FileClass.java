169a5e89b3c0e16d35c131df606dda411c896062
Altaf Quadri <altafmquadri@gmail.com>
2020-12-10 22:21:28 -0500

    add functionality of creating a physical file, deleting a file. and displaying a file

--- src/phase/end/assessment/FileClass.java
+++ src/phase/end/assessment/FileClass.java
@@ -1,57 +1,81 @@
 package phase.end.assessment;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class FileClass {
 
 	public static List<String> fileList = new ArrayList<>();
-	
+
+	public void syncLists() {
+		List<String> pathnames = new ArrayList<>();
+		File f = new File("./");
+		pathnames = Arrays.asList(f.list());
+		pathnames = pathnames.stream().filter(p -> p.endsWith(".txt")).collect(Collectors.toList());
+		fileList.clear();
+		fileList.addAll(pathnames);
+	}
+
 	public void showFileList() {
-		if (fileList.isEmpty()) System.out.println("The current directory does not contain any files");
+		System.out.println("*******************************************************************");
+		System.out.println("*******************************************************************");
+		System.out.println("*******************************************************************");
+		syncLists();
+		if (fileList.isEmpty())
+			System.out.println("The current directory does not contain any files");
 		Collections.sort(fileList, String.CASE_INSENSITIVE_ORDER);
-		fileList.forEach(f -> System.out.println(f));
+		System.out.println("###################################################################");
+		System.out.println("Your files:");
+		System.out.println("-------------------------------------------------------------------");
+		fileList.forEach(file -> System.out.println(file));
+		System.out.println("###################################################################");
 	}
-	
-	public void hardCodeText() { //capital affects it
-		fileList.add("taxes.txt");
-		fileList.add("employee.txt");
-		fileList.add("students.txt");
-		fileList.add("friends.txt");
-		fileList.add("Students.txt");
-	}
-	
-	public void add() {
+
+	public void add() throws IOException {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter the name of the file you would like to add");
+		System.out.println("*******************************************************************");
+		System.out.println("*******************************************************************");
+		System.out.println("*******************************************************************");
+		System.out.println("\nPlease enter the name of the file you would like to add:");
 		String fileName = scan.nextLine();
-		fileList.add(fileName);
-		System.out.println("The " +fileName + " file has successfully been added");
-		
+		File myFile = new File(fileName);
+		if (myFile.createNewFile()) {
+			fileList.add(fileName);
+			System.out.println("The " + fileName + " file has successfully been added");
+		} else {
+			System.out.println("This file already exists");
+		}
+		System.out.println("*******************************************************************");
+		System.out.println("*******************************************************************");
+		System.out.println("*******************************************************************");
+
 	}
-	
+
 	public void remove() {
+		System.out.println("*******************************************************************");
+		System.out.println("*******************************************************************");
+		System.out.println("*******************************************************************");
 		Scanner scan = new Scanner(System.in);
-		
 		System.out.println("Please enter the name of the file you would like to delete");
-		
+
 		String fileName = scan.nextLine();
-		
-		if (fileList.contains(fileName)) {
-			fileList.remove(fileName);
+		File myFile = new File(fileName);
+
+		if (myFile.delete()) {
+			syncLists();
 			System.out.println("The " + fileName + " was deleted!");
+
 		} else {
 			System.out.println("That file name was not found");
 		}
+	System.out.println("*******************************************************************");
+	System.out.println("*******************************************************************");
+	System.out.println("*******************************************************************");
 	}
-
-	public static List<String> getFileList() {
-		return fileList;
-	}
-
-	
-	
-	
 }
--- src/phase/end/assessment/Menu.java
+++ src/phase/end/assessment/Menu.java
@@ -1,57 +1,81 @@
 package phase.end.assessment;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Menu {
 
-	public void showMenu() {
-		System.out.println("Welcome to Company Locker Pvt Ltd...\n\n");
-		System.out.println("developed by Altaf Quadri");
-		
-		System.out.println("This program will allow you to safely store your documents"
-				+ "\nwhich you can later retrieve from anywhere.  Any documents that have\n"
-				+ "outlived their purpose can be deleted.");
-	}
-	
-	public int showChoices() {
+	public void showMainMenu() throws IOException {
 		Scanner scan = new Scanner(System.in);
-		int choice;
-		System.out.println("\n\nPlease press 1 to show files for the system");
-		System.out.println("Please press 2 to add a file");
-		System.out.println("Please press 3 to add a file");
-		
-		choice = Integer.parseInt(scan.nextLine());
 		
 		
-		return choice;
+		System.out.println("\n\n*******************************************************************");
+		System.out.println("\tWelcome to Company Locker Pvt Ltd...\n\n");
+
+		System.out.println("\tThis program will allow you to safely store your documents"
+				+ "\n\twhich you can later retrieve from anywhere.  Any documents\n"
+				+ "\tthat have outlived their purpose can be deleted.\n\n");
+
+		System.out.println("\tWould you like to enter the application y/n?");
+		System.out.println("*******************************************************************");
+		System.out.println("\t\t\t\t\tdeveloped by Altaf Quadri");
+		System.out.println("\nYour selection:\t");
+		
+		 
+		String enter = scan.nextLine().toLowerCase();
+		if (enter.equals("y")) {
+			showSubMenu();			
+		} else {
+			System.out.println("Thanks for visiting Company Locker Pvt Ltd...");
+		}
 	}
-	
+
+	public void showSubMenu() throws IOException {
+		Scanner scan = new Scanner(System.in);
+		int counter = 0;
+		while (counter ==0) {
+			System.out.println("*******************************************************************");
+			System.out.println("*******************************************************************");
+			System.out.println("\n\tPlease press (1) to show files for the system");
+			System.out.println("\tPlease press (2) to add a file");
+			System.out.println("\tPlease press (3) to delete a file");
+			System.out.println("\tPlease press (4) to return to main menu\n");
+			System.out.println("*******************************************************************");
+			System.out.println("*******************************************************************");
+			System.out.println("\nYour entry:\t");
+			int choice = Integer.parseInt(scan.nextLine());
+			
+			if (choice == 1) {
+				FileClass myFile = new FileClass();
+				myFile.showFileList();
+			}
+
+			if (choice == 2) {
+				FileClass myFile = new FileClass();
+				myFile.add();
+			}
+
+			if (choice == 3) {
+				FileClass myFile = new FileClass();
+				myFile.remove();
+			}
+			
+			if (choice == 4) {
+				counter++;
+			}
+		}
+		showMainMenu();
+	}
+
 	public static void main(String[] args) {
 		Menu menu = new Menu();
-		int selection;
-		menu.showMenu();
-		
-		selection = menu.showChoices();
-		System.out.println(selection);
-		
-		if (selection == 1) {
-			FileClass myFile = new FileClass();
-			myFile.hardCodeText();
-			myFile.showFileList();
-		}
-		
-		if (selection == 2 ) {
-			FileClass myFile = new FileClass();
-			myFile.hardCodeText();
-			myFile.add();
-			myFile.showFileList();
-		}
-		
-		if (selection == 3) {
-			FileClass myFile = new FileClass();
-			myFile.hardCodeText();
-			myFile.remove();
-			myFile.showFileList();
+		try {
+			menu.showMainMenu();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
+	
+	
